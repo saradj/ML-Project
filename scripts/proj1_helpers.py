9fdc4e8b9b27f65c346e3748069cf3926738e783
Sepideh Mamooler <39418356+sMamooler@users.noreply.github.com>
2020-10-24 12:10:21 +0200

    Add files via upload

--- scripts/proj1_helpers.py
+++ scripts/proj1_helpers.py
@@ -2,7 +2,7 @@
 """some helper functions for project 1."""
 import csv
 import numpy as np
-
+from implementations import *
 
 def load_csv_data(data_path, sub_sample=False):
     """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
@@ -23,6 +23,28 @@ def load_csv_data(data_path, sub_sample=False):
 
     return yb, input_data, ids
 
+def partition(y, data, ids, jet):
+    jet_col = 22
+    jet_indx = []
+
+    for r,row in enumerate(data):
+        if row[jet_col]==jet:
+            jet_indx.append(r)
+        
+    y_i, data_i, ids_i = y[jet_indx], data[jet_indx], ids[jet_indx]
+    print(len(jet_indx))
+   
+    return jet_indx, y_i, data_i, ids_i
+
+def remove_zero_var(data):
+    new_data = data.T
+    to_delete = []
+    for i,col in enumerate(data.T):   
+        std = np.std(col, axis=0)
+        if std==0:
+            to_delete.append(i)
+    new_data = np.delete(new_data, to_delete,0)      
+    return new_data.T
 
 def predict_labels(weights, data):
     """Generates class predictions given weights, and a test data txrix"""
@@ -31,6 +53,14 @@ def predict_labels(weights, data):
     y_pred[np.where(y_pred > 0)] = 1
     
     return y_pred
+
+def predict_labels_log_regression(y, weights, data):
+    y_pred = np.dot(data, weights)
+    y_pred[np.where(y_pred <= 0.5)] = -1
+    y_pred[np.where(y_pred > 0.5)] = 1
+    correct_percentage = np.sum(y_pred == y) / float(len(y_pred))
+    print('Percentage of correct predictions is: %', correct_percentage * 100)
+    return y_pred, correct_percentage
 
 def compute_accuracy(y_pred, y):
     """Computes accuracy"""
@@ -105,8 +135,6 @@ def replace_empty(tx):
             tx[index,i] = median                #replace the outliers with median in that column
     return tx, empty_cols
 
-
-
 def standardize(x, mean_x=None, std_x=None):
     """ standardize the dataset by subtracting the mean and deviding by std """
     if mean_x is None:
@@ -133,6 +161,148 @@ def create_csv_submission(ids, y_pred, name):
         for r1, r2 in zip(ids, y_pred):
             writer.writerow({'Id':int(r1),'Prediction':int(r2)})
 
+def compute_precision_recall(y_pred, y):
+    """compute the precison and recall"""
+    true_pos = 0
+    for i,y_n in enumerate(y_pred):
+        if y_n==y[i]:
+            true_pos+=1
+            
+    prec = true_pos / len(y_pred)
+    recall = true_pos / len(y)
+    return prec, recall
+    
+def compute_F1(y_pred, y):
+    p, r = compute_precision_recall(y_pred, y)
+    f1 = (2*p*r)/(p+r)
+    return f1
+    
+    
+def compute_accuracy(y_pred, y):
+    """Computes accuracy"""
+    sum = 0
+    for idx, y_val in enumerate(y):
+        if y_val == y_pred[idx]:
+            sum += 1
+
+    return sum / len(y)
+
+def build_poly(x, degree):
+
+    basis = np.ones((x.shape[0], 1))
+    for power in range(1, degree+1):
+        newCol = np.power(x, power)
+        basis = np.concatenate((basis, newCol), axis=1)
+
+    return basis
+
+def preprocessing(x_train):#, x_test):
+    """
+    Replace all the missing values with the median and stack the log transform of the positive columns
+    """
+    # Impute missing data
+    #x_train = x_train[:, ~np.all(x_train[1:] == x_train[:-1], axis=0)]
+    #x_test = x_test[:, ~np.all(x_test[1:] == x_test[:-1], axis=0)]
+
+    x_train, cols = replace_empty(x_train)
+    #x_train = remove_zero_var(x_train)
+    #x_test,cols_test = replace_empty(x_test)
+    
+    positive_val_cols = [] #[0, 1, 2, 3, 5, 7,8, 9, 10, 13, 16, 19, 21, 23, 26, 29]
+    for i,col in enumerate(x_train.T):
+        if (col>=0).all():
+            positive_val_cols.append(i)
+
+     #x_train_poly =  build_poly(x_train, degree)
+    # Apply log transform to the data x=>log(1+x)
+    x_train_log = np.log(1 + x_train[:, positive_val_cols])
+    x_train_sqrt = np.sqrt(x_train[:, positive_val_cols])
+    x_train_sin = np.sin(x_train)
+    x_train_cos = np.cos(x_train)
+    x_train_cross = []
+    D = x_train.shape[1]
+    
+    for i in range(D):
+        for j in range(D):
+            if i!=j:
+                x_train_cross.append(x_train[:,i]*x_train[:,j])
+             
+    x_train_cross = np.array(x_train_cross).T           
+    #x_train = np.delete(x_train, positive_val_cols, 1)
+    x_train = np.hstack((x_train, x_train_log))
+    x_train = np.hstack((x_train, x_train_sqrt))
+    x_train = np.hstack((x_train, x_train_sin))
+    x_train = np.hstack((x_train, x_train_cos))
+    x_train = np.hstack((x_train, x_train_cross))
+    #x_train = np.hstack((x_train, x_train_poly))
+    
+    #x_test_log = np.log(1 + x_test[:, positive_val_cols])
+    #x_test = np.delete(x_test, positive_val_cols, 1)
+    #x_test = np.hstack((x_test, x_test_log))
+    #x_train = np.delete(x_train,cols, axis=1)  
+    #x_test = np.delete(x_test,cols_test, axis=1)  
+
+    x_train, mean_x_train, std_x_train = standardize(x_train)
+    #x_train = remove_outliers(x_train)
+    #x_test, mean_x_test, std_x_test = standardize(x_test, mean_x_train, std_x_train)
+    #x_train = normalize(x_train)
+    #x_test = normalize(x_test)
+
+    return x_train#, x_test
+
+def remove_outliers(tX):
+    clean_data = []
+    for f in tX.T:
+        mean = np.mean(f, axis=0)
+        std = np.std(f, axis=0)
+        edge = std * 3
+        lowerb = mean - edge
+        upperb = mean + edge
+        for i,x in enumerate(f):
+            if x<lowerb:
+                f[i]=lowerb
+            elif x>upperb:
+                f[i]=upperb
+                
+    return tX
+
+def normalize(x):
+    """Standardize the original data set."""
+    min_x = np.min(x, axis=0)
+   
+    max_x = np.max(x, axis=0)
+    mindiff = x-min_x
+    diff = max_x-min_x
+
+    x[:, diff > 0]  = mindiff[:, diff > 0]/diff[ diff > 0]
+    return x
+
+def replace_empty(tx):
+    ''' replace outliers with median of the feature'''
+    empty_cols = list()             #keeps all the columns to remove if they are filled only with -999 value
+    for i in range(0,tx.shape[1]):
+        col = [x for x in tx[:,i] if x!=-999]    #removing all the empty values
+        if len(col) == 0:                         
+            empty_cols.append(i)
+        else :
+            median = np.median(col)
+            index = np.where(tx[:,i] == -999)
+            tx[index,i] = median                #replace the outliers with median in that column
+    return tx, empty_cols
+
+
+
+def standardize(x, mean_x=None, std_x=None):
+    """ standardize the dataset by subtracting the mean and deviding by std """
+    if mean_x is None:
+        mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    if std_x is None:
+        std_x = np.std(x, axis=0)
+    #x = x - mean_x
+    x[:, std_x > 0] = x[:, std_x > 0] / std_x[std_x > 0]
+
+    return x, mean_x, std_x
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
@@ -158,3 +328,69 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         end_index = min((batch_num + 1) * batch_size, data_size)
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+
+def split_data(x, y, ids, ratio, seed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(seed)
+ 
+    l = np.random.permutation(np.arange(len(y)))
+    s = int(ratio*len(y))
+    l_train = l[:s] 
+    l_test = l[s:]
+    x_train = x[l_train]  
+    x_test = x[l_test]
+    y_train = y[l_train]
+    y_test = y[l_test]
+    ids_train = ids[l_train]
+    ids_test = ids[l_test]
+    
+    return x_train, y_train, ids_train, x_test, y_test, ids_test
+    
+
+
+def build_k_indices(y, k_fold, seed):
+    """build k indices for k-fold."""
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+def compute_loss(y, tx, w):
+    pred = tx.dot(w)
+    pred[np.where(pred>=0.5)] = 1
+    pred[np.where(pred<0.5)] = -1
+    err = y - pred
+    loss= 1/2 * np.mean(err ** 2)
+    #print(loss)
+    return loss
+
+def ridge_reg_cross_validation(y, x, k_indices, k, lambda_, degree):
+    """return the loss of ridge regression."""
+    l_test = k_indices[k]
+    l_train =  k_indices[~(np.arange(k_indices.shape[0])==k)] 
+    l_train = l_train.reshape(-1)
+    x_test = x[l_test]
+    y_test = y[l_test]
+    x_train = x[l_train] 
+    y_train = y[l_train] 
+    
+    #print(x_train.shape)
+    #x_train_d = build_poly(x_train, degree)
+    #x_test_d = build_poly(x_test, degree)
+   
+    #print(x_train_d.shape)
+  
+    w, mse_loss = ridge_regression(y_train, x_train, lambda_)
+    #print(w.shape)
+    
+    loss_tr = compute_loss(y_train, x_train, w)
+    loss_te = compute_loss(y_test, x_test, w)
+    #loss_tr = compute_rmse(y_train, x_train_d, w) 
+    #loss_te = compute_rmse(y_test, x_test_d, w)  
+    return loss_tr, loss_te
+
